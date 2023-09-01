857ae2c873a76cbd47c1d0fe8926b9ddc488ef78
Olegs Pliska <olegpliska1@gmail.com>
2023-09-01 17:26:30 +0300

    Add adapter to imageFragment

--- app/src/main/java/com/opliska/chili_labs_test/ImageFragment.kt
+++ app/src/main/java/com/opliska/chili_labs_test/ImageFragment.kt
@@ -5,14 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.opliska.chili_labs_test.data.ImageModel
 import com.opliska.chili_labs_test.databinding.FragmentImageBinding
 
 class ImageFragment : Fragment() {
 
     private var _binding: FragmentImageBinding? = null
-    // This property is only valid between onCreateView and
-// onDestroyView.
     private val binding get() = _binding!!
+
+    private val imageAdapter: ImageAdapter by lazy { ImageAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +23,23 @@ class ImageFragment : Fragment() {
     ): View? {
         _binding = FragmentImageBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = binding.rvBenchmark
+        val layoutManager = GridLayoutManager(requireActivity(), 2)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = imageAdapter
+
+        val imageList: ArrayList<ImageModel> = arrayListOf()
+        imageList.add(ImageModel(imageSource = R.drawable.verstappen))
+        imageList.add(ImageModel(imageSource = R.drawable.hamilton))
+        imageList.add(ImageModel(imageSource = R.drawable.alonso))
+        imageList.add(ImageModel(imageSource = R.drawable.verstappenpng))
+
+        imageAdapter.submitList(imageList)
     }
 
     override fun onDestroyView() {
